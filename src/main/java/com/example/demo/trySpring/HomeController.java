3f26edb5a59f3c36d8fc4d8932b14f7565ac332b
tatsuya828828 <kkjy011894@gmail.com>
2020-07-07 16:05:37 +0900

    入力フォームの設置

--- src/main/java/com/example/demo/trySpring/HomeController.java
+++ src/main/java/com/example/demo/trySpring/HomeController.java
@@ -1,6 +1,9 @@
 package com.example.demo.trySpring;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
@@ -8,5 +11,12 @@ public class HomeController {
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hello";
+	}
+
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1")String str, Model model) {
+		model.addAttribute("sample", str);
+
+		return "helloResponse";
 	}
 }
