e058673a0d256da5922ccfb7607d332cd8967aa0
tatsuya828828 <kkjy011894@gmail.com>
2020-07-18 23:55:47 +0900

    Employeeクラス作成

--- src/main/java/com/example/demo/trySpring/Employee.java
+++ src/main/java/com/example/demo/trySpring/Employee.java
@@ -1,5 +1,17 @@
 package com.example.demo.trySpring;
 
-public class Employee {
+import lombok.Data;
 
+// 検索結果を入れるクラス
+// @Dataアノテーションをつけるとgetterやsetterなどを自動で作成してくれる
+// これは、Springではなくて、Lombokの機能
+@Data
+public class Employee {
+	private int employeeId; // 従業員ID
+	private String employeeName; // 従業員名
+	private int age; // 年齢
 }
+
+// リポジトリークラスやサービスクラスなどの間で渡すクラスのことを、
+// Springではドメインクラスと呼ぶ。
+// Employeeクラスもドメインクラスである
